3516c4c3b6fd6da7772d5ab5f662778414d4d1f6
sanjaygowdaat <atsanjaygowda@gmail.com>
2025-10-31 02:44:10 +0530

    LinkedList problems

--- src/LinkedList/Leetcode/LCLinkedList.java
+++ src/LinkedList/Leetcode/LCLinkedList.java
@@ -118,6 +118,62 @@ public class LCLinkedList {
         return first;
     }
 
+    //bubble sort
+    public void sortList() {
+        ListNode tempHead;
+        ListNode tempNext;
+        while (head != null) {
+            tempHead = head;
+            ListNode tempPrev = new ListNode(-1);
+            tempPrev.next = tempHead;
+            tempNext = tempHead.next;
+            if (tempHead.value > tempHead.next.value) {
+                head = head.next;
+            }
+            int counter = 0;
+            while (tempHead.next != null) {
+                if (tempHead.value > tempHead.next.value) {
+                    tempHead.next = tempHead.next.next;
+                    tempPrev.next = tempNext;
+                    tempNext.next = tempHead;
+                    counter++;
+                } else {
+                    tempHead = tempHead.next;
+                }
+                tempPrev = tempPrev.next;
+                tempNext = tempHead.next;
+            }
+            if (counter == 0) {
+                break;
+            }
+        }
+    }
+
+    // bubble sort for linked list, more efficient compared to sortList() method
+    // because im not traversing the entire list after the larger elements are placed at the end, but need size variable.
+    public void sortList2() {
+        for (int i = 0; i < size - 1; i++) {
+            ListNode temp = head;
+            ListNode tempNext = temp.next;
+            ListNode tempPrev = new ListNode(-1);
+            tempPrev.next = head;
+            if (temp.value > temp.next.value) {
+                head = head.next;
+            }
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (temp.next != null && temp.value > temp.next.value) {
+                    temp.next = temp.next.next;
+                    tempPrev.next = tempNext;
+                    tempNext.next = temp;
+                } else {
+                    temp = temp.next;
+                }
+                tempPrev = tempPrev.next;
+                tempNext = temp.next;
+            }
+        }
+    }
+
      class ListNode {
         int value;
         ListNode next;
--- src/LinkedList/Leetcode/Main.java
+++ src/LinkedList/Leetcode/Main.java
@@ -2,22 +2,34 @@ package LinkedList.Leetcode;
 
 public class Main {
     public static void main(String[] args) {
-        LCLinkedList lc = new LCLinkedList();
-        lc.add(1, 0);
-        lc.add(2, 1);
-        lc.add(3, 2);
-        lc.add(4, 3);
-        lc.display();
-        System.out.println(lc.size);
-        lc.addFirst(-1);
-        lc.addLast(5);
-        lc.display();
-        System.out.println(lc.size);
+//        LCLinkedList lc = new LCLinkedList();
+//        lc.add(1, 0);
+//        lc.add(2, 1);
+//        lc.add(3, 2);
+//        lc.add(4, 3);
+//        lc.display();
+//        System.out.println(lc.size);
+//        lc.addFirst(-1);
+//        lc.addLast(5);
+//        lc.display();
+//        System.out.println(lc.size);
+//
+//        lc.createCycle(5);
+//        System.out.println(lc.hasCycle());
+//        System.out.println("Cycle Length : " + lc.cycleLength());
+//
+//        System.out.println("Node value where cycle begins : " + lc.cycleStartNode().value);
 
-        lc.createCycle(5);
-        System.out.println(lc.hasCycle());
-        System.out.println("Cycle Length : " + lc.cycleLength());
-
-        System.out.println("Node value where cycle begins : " + lc.cycleStartNode().value);
+        LCLinkedList lc2 = new LCLinkedList();
+        lc2.add(5, 0);
+        lc2.add(4, 1);
+        lc2.add(3, 2);
+        lc2.add(2, 3);
+        lc2.add(1, 4);
+        lc2.add(5, 0);
+        lc2.display();
+        lc2.sortList2();
+        System.out.print("Sorted List: ");
+        lc2.display();
     }
 }
